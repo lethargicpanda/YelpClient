ec76026186b19dcf7591db0db3f05ae97d5554d7
Thomas Ezan <thomas@readitlater.com>
2014-03-20 21:58:44 -0700

    Initial Commit

---
+++ YelpClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YelpClient
+//
+//  Created by Thomas Ezan on 3/20/14.
+//  Copyright (c) 2014 Thomas Ezan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YelpClientTests/YelpClientTests.m
@@ -0,0 +1,34 @@
+//
+//  YelpClientTests.m
+//  YelpClientTests
+//
+//  Created by Thomas Ezan on 3/20/14.
+//  Copyright (c) 2014 Thomas Ezan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YelpClientTests : XCTestCase
+
+@end
+
+@implementation YelpClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
